02962d5f188ee53ada33b452135c6a779ad581f3
robin-krokedil <robin@krokedil.se>
2026-08-13 13:04:45 +0200

    Bumped WC tested version to 11.0.1 after test

--- woocommerce-zaver-checkout.php
+++ woocommerce-zaver-checkout.php
@@ -12,7 +12,7 @@
  * Domain Path: /languages
  *
  * WC requires at least: 6.0.0
- * WC tested up to: 10.8.1
+ * WC tested up to: 11.0.1
  * Requires Plugins: woocommerce
  *
  * License: GNU General Public License v3.0
